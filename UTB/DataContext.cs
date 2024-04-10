d268f35427154617839c38e3fca005657656c44b
DonPepe1001 <emanuel.rendon7@gmail.com>
2024-04-10 08:47:24 -0600

    base de cringe

---
+++ UTB/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using UTB.Data_Entities;
+
+namespace UTB
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Bot> Bots { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<ChatSession> ChatSessions { get; set; }
+        public DbSet<Message> Messages { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DataContext() : base("name=con")
+        {
+        }
+    }
+}
---
+++ UTB/Migrations/202404101441347_Initial.cs
@@ -0,0 +1,76 @@
+﻿namespace UTB.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class Initial : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Bots",
+                c => new
+                    {
+                        botId = c.Int(nullable: false, identity: true),
+                        botName = c.String(),
+                        description = c.String(),
+                    })
+                .PrimaryKey(t => t.botId);
+            
+            CreateTable(
+                "dbo.Categories",
+                c => new
+                    {
+                        categoryId = c.Int(nullable: false, identity: true),
+                        category = c.String(),
+                        description = c.String(),
+                    })
+                .PrimaryKey(t => t.categoryId);
+            
+            CreateTable(
+                "dbo.ChatSessions",
+                c => new
+                    {
+                        chatSessionId = c.Int(nullable: false, identity: true),
+                        sessionDate = c.DateTime(nullable: false),
+                        startTime = c.Time(nullable: false, precision: 7),
+                        endTime = c.Time(nullable: false, precision: 7),
+                        chatCategory = c.String(),
+                        result = c.String(),
+                    })
+                .PrimaryKey(t => t.chatSessionId);
+            
+            CreateTable(
+                "dbo.Messages",
+                c => new
+                    {
+                        messageId = c.Int(nullable: false, identity: true),
+                        messageDate = c.DateTime(nullable: false),
+                        content = c.String(),
+                        category = c.String(),
+                    })
+                .PrimaryKey(t => t.messageId);
+            
+            CreateTable(
+                "dbo.Users",
+                c => new
+                    {
+                        userId = c.Int(nullable: false, identity: true),
+                        username = c.String(),
+                        password = c.String(),
+                        email = c.String(),
+                    })
+                .PrimaryKey(t => t.userId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Users");
+            DropTable("dbo.Messages");
+            DropTable("dbo.ChatSessions");
+            DropTable("dbo.Categories");
+            DropTable("dbo.Bots");
+        }
+    }
+}
